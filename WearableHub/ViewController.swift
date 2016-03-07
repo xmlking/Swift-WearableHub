dc4796338e4ca1f18bc570e8256e6d179cb06042
Sumanth Chinthagunta <xmlking@gmail.com>
2016-03-06 16:18:58 -0800

    Initial Commit

---
+++ WearableHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WearableHub
+//
+//  Created by Sumanth Chinthagunta on 3/6/16.
+//  Copyright © 2016 Sumanth Chinthagunta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
